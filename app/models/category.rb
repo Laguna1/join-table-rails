d9a3ef8b90837ecbd7c6db684eb4bc48a05fcd57
Laguna1 <lagunazoomarket@gmail.com>
2021-12-22 17:03:16 +0200

    Add associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :docs_in_categs
+  has_many :users, through: :docs_in_categs
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :docs_in_categs
+  has_many :categories, through: :docs_in_categs
 end
